849c259ccb4e5117cbf47f2919cb3bc182e1ec0b
alexwood4real <akwood6@asu.edu>
2026-07-03 13:59:03 -0700

    Create Heartbeat
    Implemented a way to have the heartbeat run in the background. Changed to pulse every half second
    - cleaned up formatting
    - introduced labeling

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ bind_interrupts!(struct Irqs {
     DMA_IRQ_0 => dma::InterruptHandler<DMA_CH0>;
 });
 
+/* Async functions */
 #[embassy_executor::task]
 async fn cyw43_task(
     runner: cyw43::Runner<'static, cyw43::SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>>,
@@ -40,15 +41,36 @@ async fn cyw43_task(
     runner.run().await
 }
 
+#[embassy_executor::task]
+async fn heartbeat(
+    mut control: cyw43::Control<'static>
+) -> ! {
+    info!("Heartbeat start");
+    let delay: Duration = Duration::from_millis(500);
+
+    loop 
+        {
+        control.gpio_set(0, true).await;
+        Timer::after(delay).await;
+
+        control.gpio_set(0, false).await;
+        Timer::after(delay).await;
+        }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!("Barometer start");
 
+    /* Init RP2350 peripherals */
     let p: embassy_rp::Peripherals = embassy_rp::init(Default::default());
+
+    /* Load Wi-Fi firmware */
     let fw: &cyw43::Aligned<cyw43::A4, [u8]> = aligned_bytes!("cyw43-firmware/43439A0.bin");
     let clm: &cyw43::Aligned<cyw43::A4, [u8]> = aligned_bytes!("cyw43-firmware/43439A0_clm.bin");
     let nvram: &cyw43::Aligned<cyw43::A4, [u8]> = aligned_bytes!("cyw43-firmware/nvram_rp2040.bin");
 
+    /* Configure GPIO's and PIO/SPI */
     let pwr: Output<'_> = Output::new(p.PIN_23, Level::Low);
     let cs: Output<'_> = Output::new(p.PIN_25, Level::High);
     let mut pio: Pio<'_, PIO0> = Pio::new(p.PIO0, Irqs);
@@ -63,28 +85,34 @@ async fn main(spawner: Spawner) {
         dma::Channel::new(p.DMA_CH0, Irqs),
     );
 
+    /* Allocate state driver */
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
+
+    /* Create CYW43 driver */
     let state: &mut cyw43::State = STATE.init(cyw43::State::new());
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw, nvram).await;
+
+    /* turn on background driver */
     spawner.spawn(unwrap!(cyw43_task(runner)));
 
+    /* turn on heartbeat */
     control.init(clm).await;
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
         .await;
 
-    let delay: Duration = Duration::from_millis(250);
+    spawner.spawn(unwrap!(heartbeat(control)));
 
     /* infinite main loop */
-    loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
+    loop 
+        {
+        info!("Data collection start");
 
-        info!("led off!");
-        control.gpio_set(0, false).await;
-        Timer::after(delay).await;
-    }
+        /* Data colletion, will be done with #8 */
+        Timer::after(Duration::from_secs(1)).await;
+
+        info!("Data collection end");
+        }
 }
 
 /* Metadata */
